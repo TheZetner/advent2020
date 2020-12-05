42561fb107899cc555f43bc2c7183e4f0baebfd8
Adrian Zetner <azetner@cscscience.ca>
2020-12-05 13:04:10 -0600

    Three done

--- R/03_tobboggane.R
+++ R/03_tobboggane.R
@@ -1,6 +1,17 @@
 # Day 3
 # 20201203
 
+
+# 3 over, 1 down
+# ....#..#.................#..#..
+# #..#.#.#..#.###.#..#...#..#....
+# .#....#......#.#.#..##...#...#.
+# .............#.#..#........#.#.
+# ............##.#..#...##.###...
+# .....#..#......#......##.......
+# ........##........#...........#
+
+
 # Libraries and Setup####
 
 library(utilitarian)
@@ -8,30 +19,72 @@ libraries(dplyr, readr, tidyr, stringr, purrr)
 
 # Get Data ####
 
-i <- read_csv("data/3-input.txt", col_names = F)
+# Raw input as previewed above
+raw <- read_csv("data/3-input.txt", col_names = F)
+# raw <- read_csv("data/3-example.txt", col_names = F)
+
+# List of each pattern
+l <- map(raw$X1, ~ str_replace_all(.x, "\\.", "0") %>%
+           str_replace_all("\\#", "1") %>%
+           strsplit("") %>%
+           unlist() %>%
+           as.numeric())
+
+pat <- as.numeric(length(l[[1]]))
+
+# Matrix 323x31 of pattern
+m <- data.frame(matrix(unlist(l), ncol = pat, byrow = TRUE))
+
+# Positions defined by 3/1 slope
+df <-
+  tibble(ypos = 1:length(l),
+         xpos = 1 + 3*(ypos-1)) %>%
+  mutate(across(everything(), as.numeric))
 
 # Part A ####
 
-l <- map(i$X1, ~ str_replace_all(.x, "\\.", "0") %>%
-  str_replace_all("\\#", "1") %>%
-  strsplit("") %>%
-  unlist() %>%
-  as.numeric())
+dat <-
+  df %>%
+  tail(-1) %>%
+  mutate(xpat = if_else(xpos %% pat == 0, pat, xpos %% pat)) %>%
+  rowwise() %>%
+  mutate(val = m[ypos, xpat])
 
-x <- 1
-y <- 1
-trees <- 0
-
-for (s in 1:3) {
-  for (ss in x:x+3) {
-    if(x%%31 != 0){
-      trees <- sum(trees + l[[s]][x%%31])
-    } else{
-      trees <- sum(trees + l[[s]][31])
-    }
-  }
-  x <- x+3
-}
-
+sum(dat$val) # 237
 
 # Part B ####
+
+# Right 1, down 1.
+# Right 3, down 1. (This is the slope you already checked.)
+# Right 5, down 1.
+# Right 7, down 1.
+# Right 1, down 2.
+
+slopes <- tibble(rise = c(1, 1, 1, 1, 2),
+                 run = c(1, 3, 5, 7, 1))
+
+
+slopelist <-
+  pmap(slopes, ~ tibble(ypos = seq(1, length(l), by = ..1)) %>%  # Shorthand notation for columns in slopes
+         mutate(xpos = c(1, rep.int(..2, nrow(.)-1)),
+                xpos = cumsum(xpos)) %>%
+         mutate(across(everything(), as.numeric)))
+
+
+datlist <-
+  map(slopelist, ~ .x %>%
+        mutate(xpat = if_else(xpos %% pat == 0, pat, xpos %% pat)) %>%
+        rowwise() %>%
+        mutate(val = m[ypos, xpat]))
+
+# Sanity check. Ungroup to turn off rowwise
+datlist[[2]] %>% ungroup() %>% summarise(trees = sum(val))
+
+
+bind_rows(datlist, .id = "slope") %>%
+  group_by(slope) %>%
+  summarise(trees = sum(val)) %>%
+  summarise(prod(trees))
+
+sum(dat$val)
+
